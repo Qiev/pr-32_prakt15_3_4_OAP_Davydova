e8a1bfd5513c4adc3e0267266959f83d4485689e
Qiev <73715844+Qiev@users.noreply.github.com>
2021-11-14 16:32:03 +0500

    Add files via upload

---
+++ pr-32_prakt15_3_4_OAP_Davydova/four/four/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace four
+{
+    class Program
+    {
+        public struct person
+        {
+            public string f;
+            public string i;
+            public string o;
+            public int age;
+            public float massa;
+        }
+
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("t.txt");
+            string str;
+            Queue queue = new Queue();
+            person a;
+            Console.WriteLine("меньше 40");
+            while ((str = sr.ReadLine()) != null)
+            {
+                string[] temp = str.Split(' '); 
+                a.f = temp[0];
+                a.i = temp[1];
+                a.o = temp[2];
+                a.age = int.Parse(temp[3]);
+                a.massa = float.Parse(temp[4]);
+                if (a.age < 40)
+                    Console.WriteLine(a.f + "\t" + a.i + "\t" + a.o + "\t" + a.age + "\t" + a.massa);
+                else queue.Enqueue(a);
+            }
+            sr.Close();
+
+            Console.WriteLine("40 лет и выше");
+            while (queue.Count != 0) 
+            {
+                a = (person)queue.Dequeue();
+                Console.WriteLine(a.f + "\t" + a.i + "\t" + a.o + "\t" + a.age + "\t" + a.massa);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ pr-32_prakt15_3_4_OAP_Davydova/three/three/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n= ");
+            int a = int.Parse(Console.ReadLine());
+            Queue queue = new Queue();
+            for (int i = 1; i <= a; i++)
+                queue.Enqueue(i);
+            Console.WriteLine("Размерность очереди " + queue.Count);
+
+            Console.WriteLine("Верхний элемент очереди = " + queue.Peek());
+            Console.WriteLine("Размерность очереди " + queue.Count);
+
+            Console.Write("Содержимое очереди = ");
+            while (queue.Count != 0)
+                Console.Write("{0} ", queue.Dequeue());
+            Console.WriteLine("\nНовая размерность очереди " + queue.Count);
+            Console.Read();
+        }
+    }
+}
